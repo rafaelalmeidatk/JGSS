df7fca976c883e681bd8b33a7b5d591748b90367
Rafael Almeida <rafaelalmeidatk@gmail.com>
2017-01-16 00:04:02 -0200

    Fixed some mistakes

--- AnimationSaveBugFix.js
+++ AnimationSaveBugFix.js
@@ -1,5 +1,5 @@
 //=============================================================================
-// Animation Save Bug Fix (v1.1)
+// Animation Save Bug Fix (v1.2)
 // by Fogomax
 //=============================================================================
 
@@ -22,7 +22,7 @@
  * ===========================================================================
  * » License
  * ===========================================================================
- * WTFPL – Do What the frick You Want to Public License
+ * WTFPL – Do What the Fuck You Want to Public License
  * http://www.wtfpl.net/txt/copying/
 */
 
@@ -39,17 +39,18 @@
 		var contents = _DataManager_makeSaveContents.call(this);
 		if (contents.map) {
 			var events = contents.map._events;
-			contents.map._events = this.resolveCircularReference(events);
+			this.resolveCircularReference(events);
 		}
+		return contents;
 	};
 
-	DataManager.resolveCircularReference = function (events) {
+	DataManager.resolveCircularReference = function(events) {
 		for (var i = 0; i < events.length; i++) {
 			if (!events[i]) continue;
-			if (event._interpreter && event._interpreter._character === event) {
-				event._interpreter._character = null;
+			if (events[i]._interpreter && events[i]._interpreter._character === events[i]) {
+				events[i]._interpreter._character = null;
 			}
 		}
-	}
+	};
 
 })();
